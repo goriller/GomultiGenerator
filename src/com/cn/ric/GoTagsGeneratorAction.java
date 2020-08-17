ef1c852cbae12e2f3d9dbea1d081f0524881ef7c
linyyyang <linyyyang@tencent.com>
2020-08-17 15:33:21 +0800

    tags generatro

--- src/com/cn/ric/GoTagsGeneratorAction.java
+++ src/com/cn/ric/GoTagsGeneratorAction.java
@@ -1,18 +1,15 @@
 package com.cn.ric;
 
-import com.cn.ric.entity.StructGenerateResult;
-import com.cn.ric.util.BuilderUtil;
-import com.cn.ric.util.StructUtil;
+import com.cn.ric.util.MethodUtil;
+import com.cn.ric.util.TagsUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ScrollType;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import org.apache.commons.lang.StringUtils;
+import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -21,35 +18,35 @@ import org.jetbrains.annotations.NotNull;
  * @author: richen
  * @create: 2020-08-14 15:24
  **/
-
 public class GoTagsGeneratorAction extends AnAction {
 
-    @Override
-    public void actionPerformed(AnActionEvent e) {
-        Project project = e.getData(PlatformDataKeys.PROJECT);
 
-        final Editor editor = e.getData(PlatformDataKeys.EDITOR);
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Project project =  event.getData(PlatformDataKeys.PROJECT);
+        final Editor editor = event.getData(PlatformDataKeys.EDITOR);
+        Document document = editor.getDocument();
         if (null == editor) {
             return;
         }
 
-        String selectedText = editor.getSelectionModel().getSelectedText();
+        final CaretModel caretModel = editor.getCaretModel();
+        SelectionModel selectionModel = editor.getSelectionModel();
 
-        StructGenerateResult structGenerateResult = StructUtil.generateStruct(selectedText);
-        if (!StringUtils.isBlank(structGenerateResult.error)) {
-            Messages.showErrorDialog(project, structGenerateResult.error, "Generate Failed");
-            return;
-        }
+        final TextRange lineRange = new TextRange(
+                selectionModel.getSelectionStart(),
+                selectionModel.getSelectionEnd()
+        );
 
-        final String builderPatternCode = BuilderUtil.generateBuilderPatternCode(structGenerateResult.structEntityList);
-
-        final int textLength = editor.getDocument().getTextLength();
+        String originText = document.getText().trim();
+        final String pattern = TagsUtil.generateTagsPatternCode(originText, true, true, false);
 
         new WriteCommandAction(project) {
             @Override
             protected void run(@NotNull Result result) {
-                editor.getDocument().insertString(textLength, builderPatternCode);
-                editor.getCaretModel().moveToOffset(textLength + 2);
+                editor.getDocument().replaceString(lineRange.getStartOffset(),
+                        lineRange.getEndOffset(), pattern);
+                editor.getCaretModel().moveToOffset(caretModel.getOffset()-2);
                 editor.getScrollingModel().scrollToCaret(ScrollType.CENTER_UP);
             }
         }.execute();
